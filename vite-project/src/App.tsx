f473e0c3d933199ae432ce21d27e467dd896bbd7
Catalina Monsalve <catamon@altumlabs.co>
2023-07-19 18:06:02 -0500

    figured Authenticating inside page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -7,10 +7,25 @@ import Faq from "./components/Faq";
 import Library from "./components/Library";
 import Stripe from "./components/Stripe";
 import PlaceOrder from "./components/producer/PlaceNewOrder";
+// import "./App.css";
+import { useState, useEffect } from "react";
+import { supabase } from "./components/Authentication";
+import { Session } from "@supabase/supabase-js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
   return (
     <Router>
       <Switch>
@@ -22,10 +37,25 @@ function App() {
         <Route path="/faq" component={Faq} />
         <Route path="/library" component={Library} />
         <Route path="/stripe" component={Stripe} />
-        <Route path="/new-order" component={PlaceOrder} />
+        <Route path="/new-order">
+          <PlaceOrder session={session} />
+        </Route>
       </Switch>
     </Router>
   );
 }
 
 export default App;
+
+//   return (
+//     <div className="container" style={{ padding: "50px 0 100px 0" }}>
+//       {!session ? (
+//         <Auth />
+//       ) : (
+//         <Account key={session.user.id} session={session} />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
