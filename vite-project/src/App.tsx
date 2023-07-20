5677aaf8956569f9083caa88702ab76730f8a7c6
Ryan Ho <138708053+politecat36@users.noreply.github.com>
2023-07-20 13:59:06 -0400

    regulator dashboard

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -14,6 +14,7 @@ import { Session } from "@supabase/supabase-js";
 import ProtectedRoute from "./ProtectedRoute";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import RegulatorDashboard from "./components/RegulatorDashboard";
 
 function App() {
   const [session, setSession] = useState<Session | null>(null);
@@ -46,6 +47,7 @@ function App() {
         <Route path="/new-order">
           <PlaceOrder session={session} />
         </Route>
+        <Route path="/dashboard/regulator/" component = {RegulatorDashboard}/>
       </Switch>
     </Router>
   );
