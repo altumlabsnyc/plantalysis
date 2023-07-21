879f4580882a485be7d3911f724b4cfafcba6547
Jiaxi Wang <jiaxiwang@Jiaxis-MacBook-Pro.local>
2023-07-21 10:07:15 -0700

    refactored producer dashboard

--- vite-project/src/components/ProducerDashboard/ProdOrders.tsx
+++ vite-project/src/components/ProducerDashboard/ProdOrders.tsx
@@ -13,6 +13,7 @@ import { LabOrder, LabOrderTableRow } from "../UserTypes.js";
 import { supabase, fetchProducerOrders } from "../Authentication.js";
 import { Session } from "@supabase/supabase-js";
 import LabOrderTable from "../labDashboard/LabOrderTable.js";
+import ProducerDashboard from "./ProducerDashboard.js";
 
 interface SessionProps {
   session: Session | null;
@@ -40,113 +41,9 @@ export default function ProducerOrders({ session }: SessionProps) {
   }, [session]);
 
   return (
-    <div>
-      <div>
-        <title>Regulator Dashboard | PLANTALYSIS by ALtum Labs </title>
-        <body className="sb-nav-fixed">
-          <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            {/* Navbar Brand */}
-            <a className="navbar-brand ps-3" href="index.html">
-              PLANTALYSIS
-            </a>
-            {/* Navbar Search */}
-            <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-              {/* <div className="input-group">
-              <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-              <button className="btn btn-primary" id="btnNavbarSearch" type="button">
-                <i className="fas fa-search"></i>
-              </button>
-            </div> */}
-            </form>
-            {/* Navbar */}
-            <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdown"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <i className="fas fa-user fa-fw"></i>
-                </a>
-                <ul
-                  className="dropdown-menu dropdown-menu-end"
-                  aria-labelledby="navbarDropdown"
-                >
-                  <li>
-                    <a className="dropdown-item" href="#!">
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#!">
-                      Logout
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
-          <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-              <nav
-                className="sb-sidenav accordion sb-sidenav-dark"
-                id="sidenavAccordion"
-              >
-                <div className="sb-sidenav-menu">
-                  <div className="nav">
-                    <div className="sb-sidenav-menu-heading">Core</div>
-                    <a className="nav-link" href="/new-order">
-                      <div className="sb-nav-link-icon">
-                        <i className="fas fa-tachometer-alt"></i>
-                      </div>
-                      Place New Order
-                    </a>
-                    <a className="nav-link" href="/dashboard/producer/orders">
-                      <div className="sb-nav-link-icon">
-                        <i className="fas fa-book-open"></i>
-                      </div>
-                      Current Orders
-                    </a>
-                  </div>
-                </div>
-                <div className="sb-sidenav-footer">
-                  <div className="small">Logged in as:</div>
-                  Producer Name User
-                </div>
-              </nav>
-            </div>
-            <div id="layoutSidenav_content">
-              <main>
-                <div className="container-fluid px-4">
-                  {/* <h1 className="mt-4">Data Approval</h1>
-                <ol className="breadcrumb mb-4">
-                  <li className="breadcrumb-item active">Dashboard</li>
-                </ol> */}
-                  {<LabOrderTable labOrders={labOrders} showClaimed={false} />}
-                </div>
-              </main>
-              <footer className="py-4 bg-light mt-auto">
-                <div className="container-fluid px-4">
-                  <div className="d-flex align-items-center justify-content-between small">
-                    <div className="text-muted">
-                      Copyright &copy; PLANTALYSIS by Altum Labs 2023
-                      {/* {labOrders.map((order) => order.location)} */}
-                    </div>
-                  </div>
-                </div>
-              </footer>
-            </div>
-          </div>
-        </body>
-      </div>
-      <div></div>
-    </div>
+    <ProducerDashboard>
+      {<LabOrderTable labOrders={labOrders} showClaimed={false} />}
+    </ProducerDashboard>
   );
 }
 
--- vite-project/src/components/ProducerDashboard/ProducerDashboard.tsx
+++ vite-project/src/components/ProducerDashboard/ProducerDashboard.tsx
@@ -10,7 +10,8 @@ import "../assets/dashboard/js/datatables-simple-demo.js";
 import "../assets/dashboard/css/styles.css";
 import "https://use.fontawesome.com/releases/v6.3.0/js/all.js";
 
-const ProducerDashboard: React.FC = () => {
+const ProducerDashboard: React.FC = (props) => {
+  const {children} = props
   return (
     <div>
       <title>Producer Dashboard | PLANTALYSIS by ALtum Labs</title>
@@ -95,11 +96,7 @@ const ProducerDashboard: React.FC = () => {
           <div id="layoutSidenav_content">
             <main>
               <div className="container-fluid px-4">
-                <h1 className="mt-4">Data Approval</h1>
-                <ol className="breadcrumb mb-4">
-                  <li className="breadcrumb-item active">Dashboard</li>
-                </ol>
-                {/* table here */}
+                {children}
               </div>
             </main>
             <footer className="py-4 bg-light mt-auto">
