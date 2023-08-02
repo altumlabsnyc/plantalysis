244644b8877b0076201eeb38cfba2772fbbac05a
Grant Rinehimer <grant.rinehimer@altumlabs.co>
2023-08-02 12:44:37 -0400

    Finish send-email route

--- vite-project/src/hooks/sendEmail.ts
+++ vite-project/src/hooks/sendEmail.ts
@@ -1,14 +1,13 @@
+
 export async function sendMail(text: string) {
-  console.log('checkpoint 1')
+  console.log("checkpoint 1")
   // send mail with defined transport object
-  const response = await fetch(
-    `${import.meta.env.VITE_BACKEND_DOMAIN}/send-email`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ text }),
+  const response: Response = await fetch(`${import.meta.env.VITE_BACKEND_DOMAIN}/send-email`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  )
+    body: JSON.stringify({ text }),
+  });
+
 }
