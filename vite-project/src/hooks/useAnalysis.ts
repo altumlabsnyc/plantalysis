1e16c2724464b695a15710906cfbfd3aa35bd54c
Alex Godfrey <basilisk@altumlabs.co>
2023-08-03 15:04:59 -0400

    Test big query

--- vite-project/src/hooks/useAnalysis.ts
+++ vite-project/src/hooks/useAnalysis.ts
@@ -1,109 +1,130 @@
-import { LabOrder, TestCategory, TestResult, Analysis, RegulatorReview, TestRequirement} from '@/types/supabaseAlias'
+import {
+  Analysis,
+  RegulatorReview,
+  TestRequirement,
+} from '@/types/supabaseAlias'
 import { supabase } from '@/utils/supabase'
-import { User } from '@supabase/supabase-js'
 import useSWR from 'swr'
 
 import toast from 'react-hot-toast'
-import { RegulatorAnalysisTableRow } from '@/components/regulatorDashboard/ApproveOrders'
 
 export enum ANALYSIS_REQUEST_TYPE {
   ALL,
   APPROVED,
-  UNAPPROVED
+  UNAPPROVED,
 }
 
-export type TestResultAndReq ={
-  analysis_id: string;
-  id: string;
-  result: string;
-  test_requirement_id: string;
-  test_requirement: TestRequirement | null;
+export type TestResultAndReq = {
+  analysis_id: string
+  id: string
+  result: string
+  test_requirement_id: string
+  test_requirement: TestRequirement | null
 }
 
 export type ForApproval = {
-  lab_name: string | null | undefined,
-  producer_name: string | null | undefined,
-  test_category: string| null | undefined,
+  lab_name: string | null | undefined
+  producer_name: string | null | undefined
+  test_category: string | null | undefined
   sku: string | null
   analysis_id: string
   finished_at: string | null
   tests: Array<TestResultAndReq> | null
 }
 
-export function useAnalysis(
-// <<<<<<< HEAD
-//   user: User | null,
-//   requestType: ANALYSIS_REQUEST_TYPE | null
-// ) {
-//   const fetcher = async () => {
+export function useAnalysis() {
+  // <<<<<<< HEAD
+  //   user: User | null,
+  //   requestType: ANALYSIS_REQUEST_TYPE | null
+  // ) {
+  //   const fetcher = async () => {
 
-//     const { data, error } = await ((res) => {
-//       switch (requestType) {
-//         case ANALYSIS_REQUEST_TYPE.APPROVED:
-//           return res.not('regulator_review', 'is', null)
-//         case ANALYSIS_REQUEST_TYPE.UNAPPROVED:
-//           return res.is('regulator_review', null)
-//         default:
-//           return res
-//       }
-//     })(
-//       supabase
-//         .from('analysis')
-//         .select(`
-//       *,
-//       regulator_review (
-//         approved
-//       ),
-//       lab_order (
-//         lab_user (
-//           lab_name
-//         ),
-//         batch (
-//           producer_user (
-//             common_name
-//           )
-//         )
-//       )
-//     `)
-//     )
+  //     const { data, error } = await ((res) => {
+  //       switch (requestType) {
+  //         case ANALYSIS_REQUEST_TYPE.APPROVED:
+  //           return res.not('regulator_review', 'is', null)
+  //         case ANALYSIS_REQUEST_TYPE.UNAPPROVED:
+  //           return res.is('regulator_review', null)
+  //         default:
+  //           return res
+  //       }
+  //     })(
+  //       supabase
+  //         .from('analysis')
+  //         .select(`
+  //       *,
+  //       regulator_review (
+  //         approved
+  //       ),
+  //       lab_order (
+  //         lab_user (
+  //           lab_name
+  //         ),
+  //         batch (
+  //           producer_user (
+  //             common_name
+  //           )
+  //         )
+  //       )
+  //     `)
+  //     )
 
-//     if (error) {
-//       toast.error('Error fetching analysis. Please contact Altum Labs Support.')
-//       throw new Error('error fetching analysis data')
-//     }
+  //     if (error) {
+  //       toast.error('Error fetching analysis. Please contact Altum Labs Support.')
+  //       throw new Error('error fetching analysis data')
+  //     }
 
-//     if (!data) {
-//       throw new Error('no data returned by fetch to analysis')
-//     }
+  //     if (!data) {
+  //       throw new Error('no data returned by fetch to analysis')
+  //     }
 
-//     return data
-//   }
+  //     return data
+  //   }
 
-//   const { data, error, isLoading } = useSWR(
-//     user ? [`/api/analysis/${user.id}`, requestType] : null,
-//     fetcher
-// =======
-) {
+  //   const { data, error, isLoading } = useSWR(
+  //     user ? [`/api/analysis/${user.id}`, requestType] : null,
+  //     fetcher
+  // =======
   const fetcher = async () => {
+    let analysisData: Array<Analysis> | null
+    let analysisError: any
+    let reviewData: Array<RegulatorReview> | null
+    let reviewError: any
 
-    let analysisData: Array<Analysis> | null;
-    let analysisError: any;
-    let reviewData: Array<RegulatorReview> | null;
-    let reviewError: any;
+    const alexBigQuery = supabase
+      .from('lab_order')
+      .select(
+        `
+      *,
+      analysis ( * ),
+      lab_order_on_test ( *, 
+        test ( *, 
+          test_requirement ( *,
+            test_result ( * )
+          )
+        )
+      )
+      `,
+      )
+      .is('analysis.regulator_review_id', null)
 
     const analysisPromise = supabase
       .from('analysis')
-      .select('*').then(({data, error})=>{
-        analysisData = data;
-        analysisError = error;
+      .select('*')
+      .then(({ data, error }) => {
+        analysisData = data
+        analysisError = error
       })
 
-    const reviewedPromise = supabase.from("regulator_review").select('*').then(({data, error})=>{
-      reviewData = data;
-      reviewError = error;
-    })
+    const reviewedPromise = supabase
+      .from('regulator_review')
+      .select('*')
+      .then(({ data, error }) => {
+        reviewData = data
+        reviewError = error
+      })
 
-    await Promise.all([analysisPromise, reviewedPromise]);
+    await Promise.all([analysisPromise, reviewedPromise])
 
     const error = analysisError || reviewError
 
@@ -113,29 +134,29 @@ export function useAnalysis(
       )
       throw new Error('unable to fetch analysis data')
     }
-    
 
     //@ts-ignore
-    if (!analysisData || !reviewData){
-      toast.error(
-        'Error fetching analysis data'
-      )
-      throw new Error("unable to fetch analysis data")
+    if (!analysisData || !reviewData) {
+      toast.error('Error fetching analysis data')
+      throw new Error('unable to fetch analysis data')
     }
-    
 
-    const reviewedIdSet = new Set(reviewData.map((review)=>{
-      return review.analysis_id
-    }))
-    const notReviewedAnalysis = analysisData.filter((analysis)=>{
+    const reviewedIdSet = new Set(
+      reviewData.map((review) => {
+        return review.analysis_id
+      }),
+    )
+    const notReviewedAnalysis = analysisData.filter((analysis) => {
       return !reviewedIdSet.has(analysis.id)
     })
 
-    const labOrderDataArray: Array<ForApproval> = [];
-    const allLabOrderDataPromise: Array<Promise<void>> = [];
-    for (const analysis of notReviewedAnalysis){
-      const newPromise = supabase.from('lab_order').select(
-        `
+    const labOrderDataArray: Array<ForApproval> = []
+    const allLabOrderDataPromise: Array<Promise<void>> = []
+    for (const analysis of notReviewedAnalysis) {
+      const newPromise = supabase
+        .from('lab_order')
+        .select(
+          `
         *,
         batch (
           producer_user(
@@ -144,84 +165,84 @@ export function useAnalysis(
         ),
         lab_user (
           lab_name
-        )`
-      ).eq('id', analysis.lab_order_id).single().then(({data, error})=>{
-        if (data){
-          const column: ForApproval ={
-            analysis_id: analysis.id,
-            lab_name: data.lab_user?.lab_name,
-            producer_name: data.batch?.producer_user?.legal_name,
-            sku: analysis.lab_order_id,
-            tests: null,
-            test_category: null,
-            finished_at: analysis.finished_at
+        )`,
+        )
+        .eq('id', analysis.lab_order_id)
+        .single()
+        .then(({ data, error }) => {
+          if (data) {
+            const column: ForApproval = {
+              analysis_id: analysis.id,
+              lab_name: data.lab_user?.lab_name,
+              producer_name: data.batch?.producer_user?.legal_name,
+              sku: analysis.lab_order_id,
+              tests: null,
+              test_category: null,
+              finished_at: analysis.finished_at,
+            }
+            labOrderDataArray.push(column)
+          } else {
+            toast.error('Error fetching the data of an analysis')
           }
-          labOrderDataArray.push(column);
-        }
-        else{
-          toast.error('Error fetching the data of an analysis');
-        }
-
-      })
-      allLabOrderDataPromise.push(newPromise as Promise<void>);
+        })
+      allLabOrderDataPromise.push(newPromise as Promise<void>)
     }
 
-    await Promise.all(allLabOrderDataPromise);
+    await Promise.all(allLabOrderDataPromise)
 
-
-    
     const missingDataPromises: Array<Promise<void>> = []
-    for (const incompleteApproval of labOrderDataArray){
-      const testCategoryPromise = supabase.from('lab_order_on_test')
-      .select(`
+    for (const incompleteApproval of labOrderDataArray) {
+      const testCategoryPromise = supabase
+        .from('lab_order_on_test')
+        .select(
+          `
       test (
         test_category_name
-      )`).eq('lab_order_id', incompleteApproval.sku).single().then(({data, error})=>{
-        if (data){
-        incompleteApproval.test_category = data.test?.test_category_name
-        }
-        else{
-          toast.error("error fetching test categories")
-          //most likely reaches this point if there is no associated requirements because .single() does not work
-        }
-      })
-      missingDataPromises.push(testCategoryPromise as Promise<void>);
-      
+      )`,
+        )
+        .eq('lab_order_id', incompleteApproval.sku)
+        .single()
+        .then(({ data, error }) => {
+          if (data) {
+            incompleteApproval.test_category = data.test?.test_category_name
+          } else {
+            toast.error('error fetching test categories')
+            //most likely reaches this point if there is no associated requirements because .single() does not work
+          }
+        })
+      missingDataPromises.push(testCategoryPromise as Promise<void>)
+
       const testResultsPromise = supabase
-      .from('test_result')
-      .select(`
+        .from('test_result')
+        .select(
+          `
       *,
       test_requirement(
         *
-      )`).eq('analysis_id', incompleteApproval.analysis_id)
-      .then(({data, error})=>{
-        if (data){
-          incompleteApproval.tests = data
-        }
-        else{
-          toast.error('error fetching test results');
-        }
-      })
-      missingDataPromises.push(testResultsPromise as Promise<void>);
+      )`,
+        )
+        .eq('analysis_id', incompleteApproval.analysis_id)
+        .then(({ data, error }) => {
+          if (data) {
+            incompleteApproval.tests = data
+          } else {
+            toast.error('error fetching test results')
+          }
+        })
+      missingDataPromises.push(testResultsPromise as Promise<void>)
     }
 
-    await Promise.all(missingDataPromises);
+    await Promise.all(missingDataPromises)
 
-
-
-
-    return labOrderDataArray;
+    return labOrderDataArray
   }
 
-  const { data, error, isLoading } = useSWR(
-    `/api/analysis`,
-    fetcher,
-  )
+  const { data, error, isLoading } = useSWR(`/api/analysis`, fetcher)
 
   return {
     data: data ? data : [],
     error,
-    isLoading
+    isLoading,
   }
 }
 
--- vite-project/src/types/supabase.ts
+++ vite-project/src/types/supabase.ts
@@ -51,18 +51,21 @@ export interface Database {
           finished_at: string
           id: string
           lab_order_id: string | null
+          regulator_review_id: string | null
           started_at: string | null
         }
         Insert: {
           finished_at?: string
           id?: string
           lab_order_id?: string | null
+          regulator_review_id?: string | null
           started_at?: string | null
         }
         Update: {
           finished_at?: string
           id?: string
           lab_order_id?: string | null
+          regulator_review_id?: string | null
           started_at?: string | null
         }
         Relationships: [
@@ -70,6 +73,12 @@ export interface Database {
             foreignKeyName: "analysis_lab_order_id_fkey"
             columns: ["lab_order_id"]
             referencedRelation: "lab_order"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "analysis_regulator_review_id_fkey"
+            columns: ["regulator_review_id"]
+            referencedRelation: "regulator_review"
             referencedColumns: ["id"]
           }
         ]
@@ -889,7 +898,6 @@ export interface Database {
       }
       regulator_review: {
         Row: {
-          analysis_id: string
           approved: boolean
           created_at: string
           id: string
@@ -897,7 +905,6 @@ export interface Database {
           regulator_user_id: string
         }
         Insert: {
-          analysis_id: string
           approved?: boolean
           created_at?: string
           id?: string
@@ -905,7 +912,6 @@ export interface Database {
           regulator_user_id: string
         }
         Update: {
-          analysis_id?: string
           approved?: boolean
           created_at?: string
           id?: string
@@ -913,12 +919,6 @@ export interface Database {
           regulator_user_id?: string
         }
         Relationships: [
-          {
-            foreignKeyName: "regulator_review_analysis_id_fkey"
-            columns: ["analysis_id"]
-            referencedRelation: "analysis"
-            referencedColumns: ["id"]
-          },
           {
             foreignKeyName: "regulator_review_regulator_user_id_fkey"
             columns: ["regulator_user_id"]
