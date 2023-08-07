a05500a5e284a92b59435dbd6e1385d63613c61b
Alex Godfrey <basilisk@altumlabs.co>
2023-08-06 23:30:11 -0400

    Style

--- vite-project/src/components/producer/Order.tsx
+++ vite-project/src/components/producer/Order.tsx
@@ -41,24 +41,28 @@ export default function Order({ orderData }: OrderProps) {
   return (
     <div className="prose max-w-4xl bg-white rounded-xl p-4 mt-2 mx-auto mb-4">
       {/* <h3>Order on {parseTime(orderData.order_time, "date")}</h3> */}
-      <h3>{orderData.analysis_approved ? "Approved" : "Not Approved"}</h3>
+      <h3>{orderData.analysis_approved ? 'Approved' : 'Not Approved'}</h3>
       <div className="text-sm mt-2">
         <p className="font-bold my-0">Ordered on</p>
-        <p className="text-gray-500 my-1">{parseTime(orderData.order_time, "time")}</p>
+        <p className="text-gray-500 my-1">
+          {parseTime(orderData.order_time, 'time')}
+        </p>
         <p className="font-bold my-0">Order ID</p>
         <p className="text-gray-500 my-1">{orderData.id}</p>
         <p className="font-bold my-0">Pickup Location</p>
         <p className="text-gray-500 my-1">{orderData.location}</p>
         {orderData.analysis_id && (
           <>
-          <p className="font-bold my-0">Analysis ID </p>
-          <p className="text-gray-500 my-1">{orderData.analysis_id}</p>
-          </>)}
+            <p className="font-bold my-0">Analysis ID </p>
+            <p className="text-gray-500 my-1">{orderData.analysis_id}</p>
+          </>
+        )}
         {orderData.lab_user_id && (
-        <>
-        <p className="font-bold my-0">Lab User ID </p>
-        <p className="text-gray-500 my-1">{orderData.lab_user_id}</p>
-        </>)}
+          <>
+            <p className="font-bold my-0">Lab User ID </p>
+            <p className="text-gray-500 my-1">{orderData.lab_user_id}</p>
+          </>
+        )}
       </div>
     </div>
   )
